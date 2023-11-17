099cc3677d4f9c4291b6ee42f0684cad0c0e7ca9
DESKTOP-31VBQ8V\Puesto18 <raulcorderodiaz@gmail.com>
2023-11-17 12:41:21 +0100

    no message

--- src/main/webapp/common/js/app.js
+++ src/main/webapp/common/js/app.js
@@ -1,11 +1,57 @@
+// Container
+const container = document.querySelector(".container");
+// Botones de iniciar sesión y registrarse que aparecen en los paneles laterales
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
 
+// Recupera contraseña
+const enlaceRecuperaClave = document.getElementById("enlaceRecuperaClave"); // El enlace "¿Has olvidado la contraseña?"
+const panelLogin = document.getElementById("panel-login"); // El panel que sale en la izquierda cuando estamso en el login
+const formularioRegistro = document.getElementById("formularioRegistrarse"); // Formulario de registrarse
+const formularioRecuperaClave = document.getElementById("formularioRecuperaClave"); // Formulario de recupera contraseña
+const imagenLogin = document.getElementById("imagenLogin"); // Imagen que aparece en el panel cuando estamos en login
+
+// Método que cambiará la clase del container para cambiar entre login y register y recuperar contraseña
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
+
+  // Cambiamos el texto del h3 del panelLogin
+  panelLogin.getElementsByTagName("h3")[0].innerHTML = "¿Ya tienes una cuenta?";
+  // Cambiamos el texto del p del panelLogin
+  panelLogin.getElementsByTagName("p")[0].innerHTML = "Descubre el mundo de la lectura.";
+
+  // Ahora ocultaremos el formulario de recupera contraseña y mostraremos el formulario de registro
+  formularioRecuperaClave.style.display = "none";
+  setTimeout(function () {
+    // Mostrar el formulario de registro
+    formularioRegistro.style.display = "";
+  }, 1000);
+
+  // Cambiamos la imagen que aparece en el panel, para eso usamos src y cambiamos la foto del src
+  imagenLogin.src = "common/img/woman.png";
 });
 
+// Método que se accionará al pulsar el botón iniciar sesión
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+// Método que se accionará al pulsar sobre el enlace de "¿Has olvidado la contraseña?"
+enlaceRecuperaClave.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+
+  // Cambiamos el texto del h3 del panelLogin
+  panelLogin.getElementsByTagName("h3")[0].innerHTML = "¿Recordaste tu contraseña?";
+  // Cambiamos el texto del p y lo ponemos vacio
+  panelLogin.getElementsByTagName("p")[0].innerHTML = "";
+
+  // Ahora ocultaremos el formulario de registro y mostraremos el formulario de recupera contraseña
+  formularioRegistro.style.display = "none";
+  setTimeout(function () {
+    // Mostrar el formulario de recupera contraseña
+    formularioRecuperaClave.style.display = "";
+  }, 1000);
+
+  // Cambiamos la imagen para eso usamos src y cambiamos la foto del src
+  imagenLogin.src = "common/img/woman2.png";
+})
--- src/main/webapp/common/js/main.js
+++ src/main/webapp/common/js/main.js
@@ -1,49 +1,70 @@
-function validarEntrada(evento) {
+// Esta función se llama cuando se intenta validar una entrada, como la entrada de un usuario en un formulario.
+function validarEntrada(event) {
 	var keycode;
-	if (window.event) keycode = window.event.keyCode;
-	else if (evento) keycode = evento.which;
-	else return true;
-	if ((keycode < 48 || keycode > 57) && (keycode < 64 || keycode > 90) && (keycode < 97 || keycode > 122)) //Rango ASCII de números y letras
-		if (keycode != 8 && keycode != 13 && keycode != 27 && keycode != 0) { //Caracteres especiales permitidos
-			alert('Sólo puedes introducir letras y números');
-			return false;
-		} else if (keycode == 13)
-			enviarLogin();
-		else return true;
-	else return true;
+
+	// Utilizamos el parámetro event directamente para obtener el código de tecla
+	if (event)
+		keycode = event.which || event.keyCode;
+	else
+		return true;
+
+	// Comprobar si el código de tecla está fuera del rango de números y letras ASCII
+	if ((keycode < 48 || keycode > 57) && (keycode < 64 || keycode > 90) && (keycode < 97 || keycode > 122)) {
+
+		// Si el código de tecla no está en el rango y no es un carácter especial permitido, muestra un error y bloquea la entrada.
+		if (keycode != 8 && keycode != 13 && keycode != 27 && keycode != 0) {
+			alert('Sólo puedes introducir letras y números.');
+			return false; // Evitar que el carácter se introduzca
+		} else if (keycode == 13) {
+			enviarLogin(); // Si se presiona Enter (código 13), llamar a la función enviarLogin()
+		} else {
+			return true; // Permitir caracteres especiales (teclas de control, Enter, etc.)
+		}
+	} else {
+		return true; // Permitir letras y números
+	}
 }
 
+// Función para manejar el envío del formulario de inicio de sesión
 function enviarSignIn() {
+	// Obtener los datos del formulario de inicio de sesión
 	datos = document.signIn;
+
+	// Verificar si los campos de DNI y contraseña están vacíos
 	if (datos.dniUsuario.value == '' || datos.password.value == '')
+		// Mostrar mensaje de error si hay campos vacíos
 		document.getElementById("error2").innerHTML = 'Tienes que rellenar todos los campos.';
-	else if (!/^\d{8}[A-Za-z]$/.test(datos.dniUsuario.value)) {
+	// Verificar el formato del DNI utilizando una expresión regular
+	else if (!/^\d{8}[A-Za-z]$/.test(datos.dniUsuario.value))
 		document.getElementById("error2").innerHTML = 'El DNI no tiene un formato válido.';
-	}
+	// Enviar el formulario si todas las validaciones pasan
 	else datos.submit();
 }
 
+// Función para manejar el envío del formulario de registro
 function enviarSignUp() {
+	// Obtener los datos del formulario de registro
 	datos = document.signUp;
-	if (datos.nombreUsuario.value == '' || datos.apellidosUsuario.value == '' || datos.dniUsuario.value == '' || datos.tlfUsuario.value == '' || datos.password.value == '' || datos.email.value == '') {
+
+	if (datos.nombreUsuario.value == '' || datos.apellidosUsuario.value == '' || datos.dniUsuario.value == '' || datos.tlfUsuario.value == '' || datos.password.value == '' || datos.email.value == '')
 		document.getElementById("error1").innerHTML = 'Tienes que rellenar todos los campos.';
-	} else if (datos.nombreUsuario.value.length < 5) {
+	else if (datos.nombreUsuario.value.length < 5)
 		document.getElementById("error1").innerHTML = 'El nombre de usuario debe tener al menos 5 caracteres.';
-	} else if (!/^(?=.*\d)[A-Za-z\d]{7,}$/.test(datos.password.value)) {
+	else if (!/^(?=.*\d)[A-Za-z\d]{7,}$/.test(datos.password.value))
 		document.getElementById("error1").innerHTML = 'La contraseña debe tener al menos un número y 7 caracteres.';
-	} else if (!/^\d{8}[A-Za-z]$/.test(datos.dniUsuario.value)) {
+	else if (!/^\d{8}[A-Za-z]$/.test(datos.dniUsuario.value))
 		document.getElementById("error1").innerHTML = 'El DNI no tiene un formato válido.';
-	} else if (!/.*@.*/.test(datos.email.value)) {
+	else if (!/.*@.*/.test(datos.email.value))
 		document.getElementById("error1").innerHTML = 'El email debe contener "@".';
-	} else if (!/^\d{9}$/.test(datos.tlfUsuario.value)) {
+	else if (!/^\d{9}$/.test(datos.tlfUsuario.value))
 		document.getElementById("error1").innerHTML = 'El teléfono debe contener 9 dígitos.';
-	}
 	else
 		datos.submit();
 }
 
+// Función para manejar el envío del formulario de recuperación de contraseña por correo
 function enviarCorreo() {
-	datos = document.signIn;
+	datos = document.recuperaClave;
 	if (datos.emailUsuario.value == '') {
 		document.getElementById("resultado").innerHTML = 'Tienes que indicar un email.';
 	} else if (!/.*@.*/.test(datos.emailUsuario.value)) {
